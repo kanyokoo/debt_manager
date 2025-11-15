d781f622fbb2cb2eae933916d8a3f27ceec7aa59
kanyokoo <reubenwahome25@gmail.com>
2025-11-15 23:31:54 +0300

    FEAT: Add root URL redirect to login page

--- core/urls.py
+++ core/urls.py
@@ -1,31 +1,17 @@
-"""
-URL configuration for core project.
+# core/urls.py
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView # <-- IMPORT THIS
+from django.urls import reverse_lazy # <-- IMPORT THIS
 
 urlpatterns = [
+    # --- ADD THIS NEW PATH ---
+    # This makes the root URL redirect to the login page
+    path('', RedirectView.as_view(url=reverse_lazy('login'), permanent=False)),
+    
     path('admin/', admin.site.urls),
-    
-    # Use Django's built-in auth URLs for login, logout, password reset, etc.
     path('accounts/', include('django.contrib.auth.urls')), 
-    
-    # Include our custom account views (like register and dashboard) separately
     path('accounts/', include('accounts.urls')), 
-    
-    # Include the tracker app URLs
     path('tracker/', include('tracker.urls')),
 ]
